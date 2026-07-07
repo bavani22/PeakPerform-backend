5899ec94fa3047db7356f0fdade2ca6d05336f19
bavani22 <bavaniulax22@gmail.com>
2026-07-07 15:06:01 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -73,8 +73,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration=new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-                "http://127.0.0.1:5500",
-                "http://localhost:5500"
+                "https://bavani22.github.io/"
 
         ));
 
